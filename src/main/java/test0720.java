c3ae1c86cdb28d64440e1b334458e45d223f100b
atguigu <atguigu@atguigu.com>
2020-11-27 23:09:50 +0800

    first hot-fix

--- src/main/java/test0720.java
+++ src/main/java/test0720.java
@@ -6,6 +6,7 @@ public class test0720 {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("world");
+        System.out.println("hot-fox");
 
     }
 }
